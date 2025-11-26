b4078399187e7dfe92fcd5fde8e9fc9d1e1d9352
juanbalella20 <jbalella@fi.uba.ar>
2025-11-26 17:16:57 -0300

    feat: integrar flask Co-authored-by: Emanuel Perez <eperezm@fi.uba.ar> Co-authored-by: josValentin-fiuba <josValentin-fiuba@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # tpg-tribu-E-2025-2c
+
+en la carpeta `backend/horas/` para correr la API
+```
+pip install -r requirements.txt --break-system-packages
+```
+y para correr React en debemos estar en la carpeta `frontend/`
+```
+npm install
+npm run dev
+```
--- backend/horas/api_rest/api.py
+++ backend/horas/api_rest/api.py
@@ -1,13 +1,54 @@
 import json
 import requests
 import random
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+# IMPORTAR SERVICIOS
+from servicios.horas_controller.servicio_consulta_horas import *
+from servicios.horas_controller.servicio_registro_horas import *
+from servicios.proyecto_controller.servicio_consulta_proyectos import *
 
 RESOURCES_URL = "https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/32c8fe38-22a6-4fbb-b461-170dfac937e4/recursos-api/1.0.1/m/recursos"
 
+app = Flask(__name__)
+CORS(app)
+
+@app.get("/api/simulate/rand_emplyee_id")
 def simulate_login_id():
     data = requests.get(RESOURCES_URL).json()
-    return data[random.randint(0, len(data)-1)]["id"]
+    employee_id = data[random.randint(0, len(data)-1)]["id"]
+    return jsonify({"employee_id" : employee_id}), 200
+
+# -----------------------
+#     ENDPOINTS HORAS
+# -----------------------
+
+@app.get("/api/horas/<int:id_empleado>/<string:fecha>")
+def endpoint_horas_semanales(id_empleado, fecha):
+    data = obtener_horas_semanales(id_empleado, fecha)
+    return jsonify(data), 200
+
+
+@app.post("/api/horas")
+def endpoint_registrar_horas():
+    body = request.get_json()
+
+    resultado = registrar_horas(body)
+    return jsonify(resultado), 201
+
+
+# ------------------------------
+#   ENDPOINTS PROYECTOS / COSTOS
+# ------------------------------
+
+@app.get("/api/proyectos/<int:id_proyecto>/costos")
+def endpoint_costos_proyecto(id_proyecto):
+    anio = request.args.get("anio", type=int)
+
+    data = obtener_costos_proyecto(id_proyecto, anio)
+    return jsonify(data), 200
+
 
 if __name__ == "__main__":
-    print(simulate_login_id())
-    pass
+    app.run(debug=True, port=5000)
